3279a24b65c0a734a258a7a48be501b148aa8ce4
thanveerulaklam <thanveerulaklam@icloud.com>
2026-04-16 13:18:08 +0530

    feat: device updates

--- backend/src/routes/adms.js
+++ backend/src/routes/adms.js
@@ -10,5 +10,10 @@ router.get('/getrequest', admsGetRequest);
 router.post('/getrequest', admsGetRequest);
 router.get('/cdata', admsCdata);
 router.post('/cdata', admsCdata);
+// Some eSSL/ZKTeco firmwares use legacy .aspx endpoints.
+router.get('/getrequest.aspx', admsGetRequest);
+router.post('/getrequest.aspx', admsGetRequest);
+router.get('/cdata.aspx', admsCdata);
+router.post('/cdata.aspx', admsCdata);
 
 module.exports = router;
